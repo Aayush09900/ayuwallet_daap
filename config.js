cbff7812b0e0c2b4478af4c8d4668a88884f641d
Aayush kumar <rajaayush42@gmail.com>
2026-09-02 09:58:17 +0530

    feat: add wallet configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+window.AYU_CONFIG = {
+  chainId: '0xaa36a7',
+  chainName: 'Sepolia',
+  nativeSymbol: 'ETH',
+  contractAddress: '0x10bb66634d453ad417ee1c66811ea0c8636dc2be',
+  explorerBaseUrl: 'https://sepolia.etherscan.io',
+  appVersion: '1.0.0'
+};
